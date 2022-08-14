d0f2f7bb60c93bcbbadae13e69ac6d87be901acf
Chao Cao <1504517223@qq.com>
2022-08-14 01:46:24 -0500

    main folder

---
+++ visitors-operator/api/v1beta1/visitorsapp_types.go
@@ -0,0 +1,86 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+//DatabaseSize int32 `json:"databaseSize"`
+
+// VisitorsAppSpec defines the desired state of VisitorsApp
+//+k8s:openapi-gen=true
+type VisitorsAppSpec struct {
+	//+kubebuilder:validation:Minimum=1
+	BackendSize int32 `json:"backendSize"`
+
+	BackendAutoScaling bool `json:"backendAutoScaling"`
+
+	//+kubebuilder:validation:Minimum=30000
+	//+kubebuilder:validation:Maximum=32767
+	BackendServiceNodePort int32 `json:"backendServiceNodePort"`
+
+	FrontendTitle string `json:"frontendTitle"`
+
+	//+kubebuilder:validation:Minimum=1
+	FrontendSize int32 `json:"frontendSize"`
+
+	FrontendAutoScaling bool `json:"frontendAutoScaling"`
+
+	//+kubebuilder:validation:Minimum=30000
+	//+kubebuilder:validation:Maximum=32767
+	FrontendServiceNodePort int32 `json:"frontendServiceNodePort"`
+}
+
+// VisitorsAppStatus defines the observed state of VisitorsApp
+//+k8s:openapi-gen=true
+type VisitorsAppStatus struct {
+	BackendImage  string `json:"backendImage,omitempty"`
+	FrontendImage string `json:"frontendImage,omitempty"`
+}
+
+//+k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// VisitorsApp is the Schema for the visitorsapps API
+//+k8s:openapi-gen=true
+//+kubebuilder:subresource:status
+type VisitorsApp struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   VisitorsAppSpec   `json:"spec,omitempty"`
+	Status VisitorsAppStatus `json:"status,omitempty"`
+}
+
+//+k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+//+kubebuilder:object:root=true
+
+// VisitorsAppList contains a list of VisitorsApp
+type VisitorsAppList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []VisitorsApp `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&VisitorsApp{}, &VisitorsAppList{})
+}
---
+++ visitors-operator/controllers/visitorsapp_controller.go
@@ -0,0 +1,161 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
+
+	examplecomv1beta1 "github.com/ringdrx/visitors-operator/api/v1beta1"
+	//api "github.com/presslabs/mysql-operator/pkg/apis/mysql/v1alpha1"
+)
+
+// VisitorsAppReconciler reconciles a VisitorsApp object
+type VisitorsAppReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=example.com.my.domain,resources=visitorsapps,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=example.com.my.domain,resources=visitorsapps/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=example.com.my.domain,resources=visitorsapps/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the VisitorsApp object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
+func (r *VisitorsAppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := ctrllog.FromContext(ctx)
+	log.Info("Reconciling VisitorsApp")
+
+	// Fetch the VisitorsApp instance
+	v := &examplecomv1beta1.VisitorsApp{}
+	err := r.Get(ctx, req.NamespacedName, v)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			log.Info("VisitorsApp resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get VisitorsApp")
+		return ctrl.Result{}, err
+	}
+
+	var result *ctrl.Result
+
+	// == MySQL ==========
+
+	mysqlRunning := r.isMysqlUp(ctx, v)
+
+	if !mysqlRunning {
+		// If MySQL isn't running yet, requeue the reconcile
+		// to run again after a delay
+		delay := time.Second * time.Duration(5)
+
+		log.Info(fmt.Sprintf("MySQL isn't running, waiting for %s", delay))
+		return ctrl.Result{RequeueAfter: delay}, nil
+	}
+
+	log.Info("Database setup completed.")
+
+	// == Visitors Backend  ==========
+	result, err = r.ensureDeployment(ctx, req, v, r.backendDeployment(v))
+	if result != nil {
+		return *result, err
+	}
+
+	result, err = r.ensureService(ctx, req, v, r.backendService(v))
+	if result != nil {
+		return *result, err
+	}
+
+	err = r.updateBackendStatus(ctx, v)
+	if err != nil {
+		// Requeue the request if the status could not be updated
+		return ctrl.Result{}, err
+	}
+
+	result, err = r.handleBackendChanges(ctx, v)
+	if result != nil {
+		return *result, err
+	}
+
+	log.Info("Backend setup completed.")
+
+	// == Visitors Frontend ==========
+	result, err = r.ensureDeployment(ctx, req, v, r.frontendDeployment(v))
+	if result != nil {
+		return *result, err
+	}
+
+	result, err = r.ensureService(ctx, req, v, r.frontendService(v))
+	if result != nil {
+		return *result, err
+	}
+
+	err = r.updateFrontendStatus(ctx, v)
+	if err != nil {
+		// Requeue the request
+		return ctrl.Result{}, err
+	}
+
+	result, err = r.handleFrontendChanges(ctx, v)
+	if result != nil {
+		return *result, err
+	}
+
+	log.Info("Frontend setup completed.")
+
+	// == Finish ==========
+	// Everything went fine, don't requeue
+	log.Info("Everything went fine, don't requeue.")
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *VisitorsAppReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&examplecomv1beta1.VisitorsApp{}).
+		Owns(&appsv1.Deployment{}).
+		Owns(&appsv1.StatefulSet{}).
+		Owns(&corev1.Service{}).
+		Complete(r)
+}
